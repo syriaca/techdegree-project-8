1f31ea4c14be2b3192c23e51a28d479d78ac3188
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-07-05 11:27:45 +0200

    source maps are generated for the JavaScript and CSS files respectively.

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,26 @@ let gulp = require('gulp'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
     cleanCSS = require('gulp-clean-css'),
-    maps = require('gulp-sourcemaps'),
+    sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify');
 
 gulp.task('scripts', function(){
     return  gulp.src('js/**/*.js')
+            .pipe(sourcemaps.init())
             .pipe(concat('all.js'))
             .pipe(uglify())
             .pipe(rename('all.min.js'))
+            .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest('./dist/scripts'))
 });
 
 gulp.task('styles', function(){
     return  gulp.src('sass/global.scss')
+            .pipe(sourcemaps.init())
             .pipe(sass().on('error', sass.logError))
             .pipe(cleanCSS())
             .pipe(rename('all.min.css'))
+            .pipe(sourcemaps.write('./'))
             .pipe(gulp.dest('./dist/styles'))
 });
 
