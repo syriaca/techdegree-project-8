8d51955769e2b98a055bc54d27ae8dd8136b3597
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-07-05 11:22:38 +0200

    compile the project’s SCSS files into CSS, then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
+'use strict';
+
 let gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
     maps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify');
 
 gulp.task('scripts', function(){
-    return gulp.src('js/**/*.js')
-    .pipe(concat('all.js'))
-    .pipe(uglify())
-    .pipe(rename('all.min.js'))
-    .pipe(gulp.dest('./dist/scripts'))
+    return  gulp.src('js/**/*.js')
+            .pipe(concat('all.js'))
+            .pipe(uglify())
+            .pipe(rename('all.min.js'))
+            .pipe(gulp.dest('./dist/scripts'))
 });
 
-gulp.task('default', ['scripts'], function(){
+gulp.task('styles', function(){
+    return  gulp.src('sass/global.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(cleanCSS())
+            .pipe(rename('all.min.css'))
+            .pipe(gulp.dest('./dist/styles'))
+});
+
+gulp.task('default', ['scripts', 'styles'], function(){
     console.log('default task is done');
 });
